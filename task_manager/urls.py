4c7ad22ff1fa14fe0352df2189e6cee8ed8c8002
DzmitrySha <ravenbydev@gmail.com>
2022-10-07 22:31:18 +0300

    switch ON django admin panel

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
