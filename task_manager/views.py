973f7b5391e8e3e8cd0c59687a5192e7bd08f9d0
DzmitrySha <ravenbydev@gmail.com>
2022-10-25 00:21:32 +0300

    added login and perm mixins & logout func

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        # PermissionRequiredMixin,
+                                        )
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
@@ -31,9 +36,11 @@ class CreateUser(CreateView):
                      }
 
 
-class UpdateUser(UpdateView):
+class UpdateUser(LoginRequiredMixin,
+                 # PermissionRequiredMixin,
+                 UpdateView):
     model = User
-    # form_class =
+    # form_class = UserChange
     fields = ['username', 'first_name', 'last_name', 'email', 'password']
     template_name = "update.html"
     success_url = reverse_lazy("login")
@@ -41,18 +48,25 @@ class UpdateUser(UpdateView):
     extra_context = {'title': 'Изменение пользователя',
                      'btn_name': 'Изменить',
                      }
+    # permission_required = ('task_manager.view_choice',
+    #                        'task_manager.change_choice'
+    #                        )
 
 
-class DeleteUser(DeleteView):
+class DeleteUser(LoginRequiredMixin,
+                 # PermissionRequiredMixin,
+                 DeleteView):
     model = User
     template_name = "delete.html"
     success_url = reverse_lazy("users")
     # extra_context = {'title': _('Delete user'), }
-    extra_context = {'title': 'Удаление пользователя', }
+    extra_context = {'title': 'Удаление пользователя',
+                     'btn_name': 'Да, удалить',
+                     }
+    # permission_required = 'task_manager.delete_user'
 
 
 class LoginUser(LoginView):
-    # model = User
     form_class = AuthenticationForm
     template_name = "login.html"
     success_url = reverse_lazy("users")
@@ -61,8 +75,10 @@ class LoginUser(LoginView):
                      'btn_name': 'Войти',
                      }
 
+    def get_success_url(self):
+        return reverse_lazy('home')
 
-class LogoutUser(LogoutView):
-    model = User
-    form_class = AuthenticationForm
-    success_url = reverse_lazy("home")
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
