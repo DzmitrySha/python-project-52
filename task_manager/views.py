9568afedf66708c0cc0871c345ae7b5f43daa07c
DzmitrySha <ravenbydev@gmail.com>
2022-10-27 22:09:25 +0300

    added i18n translate (en)

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,5 @@
-from django.contrib.auth import logout, login
+from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
-# from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
@@ -16,14 +15,17 @@ class UsersList(ListView):
     model = User
     template_name = "users.html"
     context_object_name = "users"
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['title'] = _('Users')
-        context['title'] = 'Пользователи'
-        context['btn_update'] = 'Изменить'
-        context['btn_delete'] = 'Удалить'
-        return context
+    extra_context = {'title': _('Users'),
+                     'btn_update': _('Update'),
+                     'btn_delete': _('Delete'),
+                     }
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context.update({'title': _('Users'),
+    #                     'btn_update': _('Update'),
+    #                     'btn_delete': _('Delete'),
+    #                     })
+    #     return context
 
 
 class CreateUser(CreateView):
@@ -31,12 +33,8 @@ class CreateUser(CreateView):
     template_name = "create.html"
     success_url = reverse_lazy("login")
     extra_context = {'title': _('Registration user'),
-                     'btn_name': _('Зарегистрировать')
+                     'btn_name': _('Register')
                      }
-    # extra_context = {'title': 'Регистрация пользователя',
-    #                  'btn_name': 'Зарегистрировать',
-    #                  }
-
     # автоматический вход пользователя после регистрации:
     # def form_valid(self, form):
     #     user = form.save()
@@ -50,9 +48,8 @@ class UpdateUser(UserPermissionsMixin, UpdateView):
     template_name = "update.html"
     login_url = "login"
     success_url = reverse_lazy("login")
-    # extra_context = {'title': _('Update user'), }
-    extra_context = {'title': 'Изменение пользователя',
-                     'btn_name': 'Изменить',
+    extra_context = {'title': _('Update user'),
+                     'btn_name': _('Update'),
                      }
     # permission_required = ('task_manager.view_user',
     #                        'task_manager.change_user'
@@ -63,9 +60,8 @@ class DeleteUser(UserPermissionsMixin, DeleteView):
     model = User
     template_name = "delete.html"
     success_url = reverse_lazy("users")
-    # extra_context = {'title': _('Delete user'), }
-    extra_context = {'title': 'Удаление пользователя',
-                     'btn_name': 'Да, удалить',
+    extra_context = {'title': _('Delete user'),
+                     'btn_name': _('Yes, delete'),
                      }
     # permission_required = ('task_manager.view_user',
     #                        'task_manager.delete_user'
@@ -75,9 +71,8 @@ class DeleteUser(UserPermissionsMixin, DeleteView):
 class LoginUser(LoginView):
     form_class = AuthenticationForm
     template_name = "login.html"
-    # extra_context = {'title': _('Enter'), }
-    extra_context = {'title': 'Вход',
-                     'btn_name': 'Войти',
+    extra_context = {'title': _('Enter'),
+                     'btn_name': _('Enter'),
                      }
 
     def get_success_url(self):
