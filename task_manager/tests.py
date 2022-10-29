1a0cdbe4b14372252736202d00f5c9956d9ba55b
DzmitrySha <ravenbydev@gmail.com>
2022-10-29 11:35:27 +0300

    added tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,7 +1,37 @@
-# import pytest
 from django.test import TestCase
 from django.contrib.auth.models import User
 
+
+class UserTestCase(TestCase):
+    # fixtures = ['test_data.json', 'moar_data.json']
+
+    def setUp(self):
+        User.objects.create(username="Kostya",
+                            first_name="Konstantin",
+                            last_name="Konstantinopolsky",
+                            email="k_email@mail.com"
+                            )
+        User.objects.create(username="user1",
+                            first_name="Fakename",
+                            )
+
+    def test_create_user(self):
+        user = User.objects.get(username="Kostya")
+        self.assertEqual(user.first_name, 'Konstantin')
+        self.assertEqual(user.last_name, 'Konstantinopolsky')
+        self.assertEqual(user.email, 'k_email@mail.com')
+
+    def test_delete_user(self):
+        user1 = User.objects.get(username="user1")
+        user1.delete()
+        self.assertEqual(user1.id, None)
+        self.assertEqual(user1.pk, None)
+        with self.assertRaises(user1.DoesNotExist):
+            User.objects.get(pk=2)
+
+
+# import pytest
+#
 #
 # @pytest.mark.django_db
 # @pytest.fixture
@@ -21,18 +51,3 @@ from django.contrib.auth.models import User
 #     assert user_admin.first_name == "Admin2"
 #     assert user_admin.last_name == "Petrovich2"
 #
-
-
-class UserTestCase(TestCase):
-    def setUp(self):
-        User.objects.create(username="Kostya",
-                            first_name="Konstantin",
-                            last_name="Konstantinopolsky",
-                            email="k_email@mail.com"
-                            )
-
-    def test_create_user(self):
-        user = User.objects.get(username="Kostya")
-        self.assertEqual(user.first_name, 'Konstantin')
-        self.assertEqual(user.last_name, 'Konstantinopolsky')
-        self.assertEqual(user.email, 'k_email@mail.com')
