05ea71fc976f0d53c847ab7fbcf67cfb99159119
DzmitrySha <ravenbydev@gmail.com>
2022-11-01 00:17:48 +0300

    added verbose names

--- task_manager/models.py
+++ task_manager/models.py
@@ -4,5 +4,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TaskStatus(models.Model):
-    name = models.CharField(max_length=120, blank=False)
-    created_date = models.DateTimeField(_("Created date"), default=timezone.now)
+    name = models.CharField(max_length=120, blank=False, verbose_name=_("Name"))
+    created_date = models.DateTimeField(verbose_name=_("Created date"),
+                                        default=timezone.now)
+
+    def __str__(self):
+        return self.name
