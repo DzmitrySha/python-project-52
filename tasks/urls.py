bf88c83b832d952003024982dbaa5162c9397e21
DzmitrySha <ravenbydev@gmail.com>
2022-11-01 23:54:34 +0300

    added urls and views with login permissions

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from users.views import (UsersList,
+from tasks.views import (TasksList,
                          # CreateTask, UpdateTask,
                          # DeleteTask, ViewTask
                          )
 
 urlpatterns = [
-    # path('', TasksList.as_view(), name="tasks"),
-    # path('create/', CreateTask.as_view(), name="create"),
-    # path('<int:pk>/update/', UpdateTask.as_view(), name="update"),
-    # path('<int:pk>/delete/', DeleteTask.as_view(), name="delete"),
-    # path('<int:pk>/', ViewTask.as_view(), name="view"),
+    path('', TasksList.as_view(), name="tasks"),
+    # path('create/', CreateTask.as_view(), name="task_create"),
+    # path('<int:pk>/update/', UpdateTask.as_view(), name="task_update"),
+    # path('<int:pk>/delete/', DeleteTask.as_view(), name="task_delete"),
+    # path('<int:pk>/', ViewTask.as_view(), name="task_view"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils.translation import gettext_lazy as _
+
+from tasks.permissions import TasksLoginRequiredMixin
+from tasks.models import Tasks
+
 
 # Create your views here.
+
+class TasksList(TasksLoginRequiredMixin, ListView):
+    model = Tasks
+    template_name = "tasks/tasks.html"
+    context_object_name = "tasks"
+    extra_context = {'title': _('Tasks'),
+                     'btn_update': _('Update'),
+                     'btn_delete': _('Delete'),
+                     }
+
+
+# class OneTaskList(DetailView):
