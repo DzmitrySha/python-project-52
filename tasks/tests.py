4665657453586edf0df9253f828b068b1b2ef595
DzmitrySha <ravenbydev@gmail.com>
2022-11-02 00:02:48 +0300

    added tasks tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from tasks.models import Tasks
 
-# Create your tests here.
+
+class TasksTest(TestCase):
+    def setUp(self):
+        Tasks.objects.create(name="task 1",)
+        Tasks.objects.create(name="task 2",)
+
+    def test_create_status(self):
+        task_3 = Tasks.objects.create(name="task created")
+        self.assertEqual(task_3.name, "task created")
+
+    def test_update_status(self):
+        task_2 = Tasks.objects.get(name="task 2")
+        task_2.name = "another name"
+        self.assertNotEqual(task_2.name, "task 2")
+
+    def test_delete_status(self):
+        task_1 = Tasks.objects.get(name="task 1")
+        task_1.delete()
+        self.assertEqual(task_1.id, None)
+        with self.assertRaises(Tasks.DoesNotExist):
+            Tasks.objects.get(pk=8)
