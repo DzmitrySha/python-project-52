d6dc4ad141c66bcdf893cf79533b3271c07c96b4
DzmitrySha <ravenbydev@gmail.com>
2022-11-03 01:07:19 +0300

    added fields to Tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+from statuses.models import TaskStatus
 
 
 # Create your models here.
 class Tasks(models.Model):
     name = models.CharField(max_length=120, verbose_name=_('Name'), blank=False)
     description = models.TextField(verbose_name=_('Description'), blank=True)
-# человек на которого задача назначена. Подразумевается, что этот человек её выполняет.
-    # executor =
-# обязательные поля - автор(устанавливается автоматически при создании задачи) и статус
-    # author =
-    # status =
+
+    # Человек, на которого задача назначена.
+    # Подразумевается, что этот человек её выполняет.
+    # executor = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # обязательные поля - автор(устанавливается автоматически
+    # при создании задачи) и статус
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               default=None, blank=False)
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE,
+                               default=None, blank=True)
