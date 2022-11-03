5a87013b4be954d8a108a527ffc7bd1c27eaadb4
DzmitrySha <ravenbydev@gmail.com>
2022-11-03 19:04:50 +0300

    added ForeignKey fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
-from statuses.models import TaskStatus
 
 
-# Create your models here.
 class Tasks(models.Model):
     name = models.CharField(max_length=120, verbose_name=_('Name'), blank=False)
     description = models.TextField(verbose_name=_('Description'), blank=True)
 
-    # Человек, на которого задача назначена.
-    # Подразумевается, что этот человек её выполняет.
-    # executor = models.OneToOneField(User, on_delete=models.CASCADE)
+    executor = models.ForeignKey(
+        to=User, on_delete=models.PROTECT, blank=True, default=None,
+    )
+    author = models.ForeignKey(
+        to=User, on_delete=models.PROTECT, blank=False, default=None,
+        related_name='authors',
+    )
+    status = models.ForeignKey(
+        to='statuses.TaskStatus', on_delete=models.PROTECT, blank=True,
+        default=None, related_name='statuses',
+    )
 
-    # обязательные поля - автор(устанавливается автоматически
-    # при создании задачи) и статус
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               default=None, blank=False)
-    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE,
-                               default=None, blank=True)
+    def __str__(self):
+        return self.name
