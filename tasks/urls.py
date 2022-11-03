21b0664f150cd4f18c69ce1ec74e23923738a4ef
DzmitrySha <ravenbydev@gmail.com>
2022-11-03 20:15:50 +0300

    added CRUD paths

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-
-from tasks.views import (TasksList,
-                         # CreateTask, UpdateTask,
-                         # DeleteTask, ViewTask
-                         )
+from tasks.views import (
+    TasksList, OneTaskView, CreateTask, UpdateTask, DeleteTask,
+)
 
 urlpatterns = [
     path('', TasksList.as_view(), name="tasks"),
-    # path('create/', CreateTask.as_view(), name="task_create"),
-    # path('<int:pk>/update/', UpdateTask.as_view(), name="task_update"),
-    # path('<int:pk>/delete/', DeleteTask.as_view(), name="task_delete"),
-    # path('<int:pk>/', ViewTask.as_view(), name="task_view"),
+    path('create/', CreateTask.as_view(), name="task_create"),
+    path('<int:pk>/update/', UpdateTask.as_view(), name="task_update"),
+    path('<int:pk>/delete/', DeleteTask.as_view(), name="task_delete"),
+    path('<int:pk>/', OneTaskView.as_view(), name="task_view"),
 ]
