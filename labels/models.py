3091a94c8ac0e7b0932a12a920d11c2c3b08c7e2
DzmitrySha <ravenbydev@gmail.com>
2022-11-07 19:35:55 +0300

    created labels model

--- labels/models.py
+++ labels/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Labels(models.Model):
+    name = models.CharField(max_length=100)
+    created_date = models.DateTimeField(verbose_name=_("Created date"),
+                                        default=timezone.now)
