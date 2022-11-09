ba1c1d0762ae5a23b64a5a383af2d6562ccc2f15
DzmitrySha <ravenbydev@gmail.com>
2022-11-09 23:40:09 +0300

    modified DeleteLabel view (try-except: ProtectedError)

--- labels/views.py
+++ labels/views.py
@@ -1,18 +1,19 @@
 from django.contrib import messages
+from django.db.models import ProtectedError
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import (ListView, CreateView, UpdateView, DeleteView)
 
 from labels.models import Label
 from labels.permissions import LabelLoginRequiredMixin
-from tasks.models import Tasks
 
 
 class LabelsList(LabelLoginRequiredMixin, ListView):
     model = Label
     template_name = "labels/labels.html"
-    context_object_name = "labels"
     login_url = "login"
+    context_object_name = "labels"
     extra_context = {'title': _('Labels'),
                      'btn_update': _('Update'),
                      'btn_delete': _('Delete'),
@@ -51,23 +52,21 @@ class DeleteLabel(LabelLoginRequiredMixin, DeleteView):
     model = Label
     template_name = "labels/delete.html"
     login_url = "login"
+    success_url = reverse_lazy('labels')
     context_object_name = "label"
     extra_context = {'title': _('Delete label'),
                      'btn_name': _('Yes, delete'),
                      }
 
-    def get_success_url(self):
-        messages.info(self.request, _('Label successfully deleted'))
-        return reverse_lazy('labels')
-        # messages.info(self.request, _('Невозможно удалить метку, потому что она используется'))
-
-    # def form_valid(self, form):
-    #     label = Label.objects.get()
-    #     for task in Tasks.objects.all():
-    #         if label in task.labels.all():
-    #             messages.info(
-    #                 self.request,
-    #                 _('Невозможно удалить метку, потому что она используется')
-    #             )
-    #             return reverse_lazy('labels')
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        try:
+            self.object.delete()
+            messages.info(self.request, _('Label successfully deleted'))
+        except ProtectedError:
+            messages.error(
+                self.request,
+                _('It`s not possible to delete the label that is being used')
+            )
+        # return HttpResponseRedirect(success_url)
+        return redirect(success_url)
