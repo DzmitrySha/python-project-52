428be34376b9c560a75a6df7351b843496a86b14
DzmitrySha <ravenbydev@gmail.com>
2022-11-11 21:20:34 +0300

    added Railway app domain to ALLOWED_HOST

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = ['0.0.0.0',
                  'localhost',
                  '127.0.0.1',
                  'task-manager-4.herokuapp.com',
-                 'webserver'
+                 'python-project-52-production-af44.up.railway.app',
+                 'webserver',
                  ]
 
 # Application definition
