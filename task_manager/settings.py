a5e3b8833004c275f0d7a428d0b999340968eb95
DzmitrySha <ravenbydev@gmail.com>
2022-11-19 11:44:53 +0300

    update settings.py (env variables)

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',)
+SECRET_KEY = os.environ.get('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
+DEBUG = bool(os.environ.get('DEBUG', False))
 
 ALLOWED_HOSTS = ['0.0.0.0',
                  'localhost',
