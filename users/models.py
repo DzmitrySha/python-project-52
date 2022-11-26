251e9982449df0be4a0d96f9b510dab3cb15f506
DzmitrySha <ravenbydev@gmail.com>
2022-11-26 21:39:23 +0300

    update user model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.get_full_name()
