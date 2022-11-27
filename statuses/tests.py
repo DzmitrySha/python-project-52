a8c1ffb940fa5904bf7d3e927de153b2b25055ef
DzmitrySha <ravenbydev@gmail.com>
2022-11-27 17:11:11 +0300

    modified statuses tests, added fixtures

--- statuses/tests.py
+++ statuses/tests.py
@@ -3,22 +3,20 @@ from statuses.models import TaskStatus
 
 
 class StatusesTest(TestCase):
-    def setUp(self):
-        TaskStatus.objects.create(name="status 1",)
-        TaskStatus.objects.create(name="status 2",)
+    fixtures = ['statuses.json']
 
     def test_create_status(self):
-        status_3 = TaskStatus.objects.create(name="status created")
-        self.assertEqual(status_3.name, "status created")
+        status = TaskStatus.objects.create(name="status created")
+        self.assertEqual(status.name, "status created")
 
     def test_update_status(self):
-        status_2 = TaskStatus.objects.get(name="status 2")
-        status_2.name = "another name"
-        self.assertNotEqual(status_2.name, "status 2")
+        status = TaskStatus.objects.get(pk=2)
+        status.name = "another name"
+        self.assertNotEqual(status.name, "status 2")
 
     def test_delete_status(self):
-        status_1 = TaskStatus.objects.get(name="status 1")
-        status_1.delete()
-        self.assertEqual(status_1.id, None)
+        status = TaskStatus.objects.get(pk=1)
+        status.delete()
+        self.assertEqual(status.id, None)
         with self.assertRaises(TaskStatus.DoesNotExist):
             TaskStatus.objects.get(pk=8)
