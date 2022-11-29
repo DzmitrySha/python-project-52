085a5e6870bae6356f139b3728a3961b91d73b12
DzmitrySha <ravenbydev@gmail.com>
2022-11-29 23:05:22 +0300

    added SuccessMessageMixin and LogoutUser

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from task_manager import settings
-from task_manager.views import IndexView, LoginUser, logout_user
+from task_manager.views import IndexView, LoginUser, LogoutUser
 
 urlpatterns = [
     path('', IndexView.as_view(), name="home"),
     path('admin/', admin.site.urls, name="admin"),
     path('users/', include('users.urls')),
     path('login/', LoginUser.as_view(), name="login"),
-    path('logout/', logout_user, name="logout"),
+    path('logout/', LogoutUser.as_view(), name="logout"),
     path('statuses/', include('statuses.urls')),
     path('labels/', include('labels.urls')),
     path('tasks/', include('tasks.urls')),
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,33 +1,32 @@
 from django.contrib import messages
-from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
 
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = 'index.html'
     extra_context = {'title': _('Task manager'),
                      'description': _('A simple and functional task manager.'),
                      }
 
 
-class LoginUser(LoginView):
+class LoginUser(SuccessMessageMixin, LoginView):
     form_class = AuthenticationForm
-    template_name = "users/form.html"
+    template_name = 'users/form.html'
+    success_message = _('You are logged in.')
     extra_context = {'title': _('Login'),
                      'btn_name': _('Enter'),
                      }
 
     def get_success_url(self):
-        messages.info(self.request, _('You are logged in.'))
         return reverse_lazy('home')
 
 
-def logout_user(request):
-    logout(request)
-    messages.info(request, _('You are logged out.'))
-    return redirect('home')
+class LogoutUser(LogoutView):
+    def get_success_url(self):
+        messages.success(self.request, _('You are logged out.'))
+        return reverse_lazy('home')
