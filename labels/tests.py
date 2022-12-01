f284cfc5e2f07f62148180a67ae8d6d219bc814e
DzmitrySha <ravenbydev@gmail.com>
2022-12-01 23:32:25 +0300

    update labels tests, add fixture

--- labels/tests.py
+++ labels/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from labels.models import Label
 
@@ -6,17 +8,21 @@ class LabelsTest(TestCase):
     fixtures = ['labels.json']
 
     def test_create_label(self):
-        label_3 = Label.objects.create(name="label created")
-        self.assertEqual(label_3.name, "label created")
+        data_label = json.load(open("fixtures/label.json"))
+        label = Label.objects.create(**data_label)
+        self.assertEqual(label.name, data_label.get("name"))
 
     def test_update_label(self):
-        label_2 = Label.objects.get(name="label 2")
-        label_2.name = "another name"
-        self.assertNotEqual(label_2.name, "label 2")
+        data_label = json.load(open("fixtures/label.json"))
+        label = Label.objects.get(pk=2)
+        self.assertNotEqual(label.name, data_label.get("name"))
+
+        label.name = data_label.get("name")
+        self.assertEqual(label.name, data_label.get("name"))
 
     def test_delete_label(self):
-        label_1 = Label.objects.get(name="label 1")
-        label_1.delete()
-        self.assertEqual(label_1.id, None)
+        label = Label.objects.get(pk=1)
+        label.delete()
+        self.assertEqual(label.id, None)
         with self.assertRaises(Label.DoesNotExist):
             Label.objects.get(pk=8)
