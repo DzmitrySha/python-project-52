fc4c0a6141026796fc62d1681d66333edc102bbf
DzmitrySha <ravenbydev@gmail.com>
2022-12-01 22:50:39 +0300

    update users tests, add fixture

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 from users.models import User
 
@@ -6,28 +7,31 @@ class UsersTest(TestCase):
     fixtures = ['users.json']
 
     def test_create_user(self):
-        user = User.objects.create(username="kep", first_name="Kevin",
-                                   last_name="Phillips", email="kep@mail.com",
-                                   )
-        self.assertEqual(user.username, 'kep')
-        self.assertEqual(user.first_name, 'Kevin')
-        self.assertEqual(user.last_name, 'Phillips')
-        self.assertEqual(user.email, 'kep@mail.com')
+        user_data = json.load(open("fixtures/user.json"))
+        user = User.objects.create(**user_data)
+
+        self.assertEqual(user.username, user_data.get('username'))
+        self.assertEqual(user.first_name, user_data.get('first_name'))
+        self.assertEqual(user.last_name, user_data.get('last_name'))
+        self.assertEqual(user.email, user_data.get('email'))
 
     def test_update_user(self):
+        user_data = json.load(open("fixtures/user.json"))
         user1 = User.objects.get(pk=1)
 
-        self.assertEqual(user1.username, "albert")
-        self.assertEqual(user1.first_name, "Albert")
-        self.assertEqual(user1.last_name, "Einstein")
+        self.assertNotEqual(user1.username, user_data.get('username'))
+        self.assertNotEqual(user1.first_name, user_data.get('first_name'))
+        self.assertNotEqual(user1.last_name, user_data.get('last_name'))
 
-        user1.username = "harry"
-        user1.first_name = "Harry"
-        user1.last_name = "Potter"
+        user1.username = user_data.get('username')
+        user1.first_name = user_data.get('first_name')
+        user1.last_name = user_data.get('last_name')
+        user1.email = user_data.get('email')
 
-        self.assertEqual(user1.username, "harry")
-        self.assertEqual(user1.first_name, "Harry")
-        self.assertEqual(user1.last_name, "Potter")
+        self.assertEqual(user1.username, user_data.get('username'))
+        self.assertEqual(user1.first_name, user_data.get('first_name'))
+        self.assertEqual(user1.last_name, user_data.get('last_name'))
+        self.assertEqual(user1.email, user_data.get('email'))
 
     def test_delete_user(self):
         user = User.objects.get(pk=2)
