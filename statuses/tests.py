ca5c932e947d294c4bdd6691ab3a29f65873affc
DzmitrySha <ravenbydev@gmail.com>
2022-12-04 13:21:33 +0300

    update statuses tests.py, add status fixture

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 from statuses.models import TaskStatus
 
@@ -6,17 +7,22 @@ class StatusesTest(TestCase):
     fixtures = ['statuses.json']
 
     def test_create_status(self):
-        status = TaskStatus.objects.create(name="status created")
-        self.assertEqual(status.name, "status created")
+        data_status = json.load(open("fixtures/status.json"))
+        status = TaskStatus.objects.create(**data_status)
+        self.assertEqual(status.name, data_status.get("name"))
 
     def test_update_status(self):
+        data_status = json.load(open("fixtures/status.json"))
+
         status = TaskStatus.objects.get(pk=2)
-        status.name = "another name"
-        self.assertNotEqual(status.name, "status 2")
+        self.assertNotEqual(status.name, data_status.get("name"))
+
+        status.name = data_status.get("name")
+        self.assertEqual(status.name, data_status.get("name"))
 
     def test_delete_status(self):
         status = TaskStatus.objects.get(pk=1)
         status.delete()
         self.assertEqual(status.id, None)
         with self.assertRaises(TaskStatus.DoesNotExist):
-            TaskStatus.objects.get(pk=8)
+            TaskStatus.objects.get(pk=1)
