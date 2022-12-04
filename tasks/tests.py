7d2e5338306981b80d62438e49bf3ebee8200c1c
DzmitrySha <ravenbydev@gmail.com>
2022-12-04 17:23:46 +0300

    update tasks tests.py, add tasks fixtures

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 from users.models import User
 from statuses.models import TaskStatus
@@ -5,31 +6,46 @@ from tasks.models import Tasks
 
 
 class TasksTest(TestCase):
-    def setUp(self):
-        user = User.objects.create(username='Harry')
-        status = TaskStatus.objects.create(name='Done')
-        Tasks.objects.create(name="task 1", author=user,
-                             status=status, executor=user
-                             )
-        Tasks.objects.create(name="task 2", author=user,
-                             status=status, executor=user
-                             )
+    fixtures = ["users.json", "statuses.json", "tasks.json"]
 
     def test_create_task(self):
-        user = User.objects.create(username='Harry2')
-        status = TaskStatus.objects.create(name='Done2')
-        task_3 = Tasks.objects.create(name="task created",
-                                      author=user, status=status, executor=user)
-        self.assertEqual(task_3.name, "task created")
+        data_task = json.load(open("fixtures/one_task.json"))
+        task = Tasks.objects.create(**data_task)
+        self.assertEqual(task.name, data_task.get("name"))
+        self.assertEqual(task.author_id, data_task.get("author_id"))
+        self.assertEqual(task.executor_id, data_task.get("executor_id"))
 
     def test_update_task(self):
-        task_2 = Tasks.objects.get(name="task 2")
-        task_2.name = "another name"
-        self.assertNotEqual(task_2.name, "task 2")
+        data_task = json.load(open("fixtures/one_task.json"))
+        task = Tasks.objects.get(pk=1)
+
+        new_name = data_task.get("name")
+        new_author_id = data_task.get("author_id")
+        new_executor_id = data_task.get("executor_id")
+        new_status_id = data_task.get("status_id")
+        new_description = data_task.get("description")
+
+        self.assertNotEqual(task.name, new_name)
+        self.assertNotEqual(task.author_id, new_author_id)
+        self.assertNotEqual(task.executor_id, new_executor_id)
+        self.assertNotEqual(task.status_id, new_status_id)
+        self.assertNotEqual(task.description, new_description)
+
+        task.name = new_name
+        task.author_id = new_author_id
+        task.executor_id = new_executor_id
+        task.status_id = new_status_id
+        task.description = new_description
+
+        self.assertEqual(task.name, new_name)
+        self.assertEqual(task.author_id, new_author_id)
+        self.assertEqual(task.executor_id, new_executor_id)
+        self.assertEqual(task.status_id, new_status_id)
+        self.assertEqual(task.description, new_description)
 
     def test_delete_task(self):
-        task_1 = Tasks.objects.get(name="task 1")
-        task_1.delete()
-        self.assertEqual(task_1.id, None)
+        task = Tasks.objects.get(pk=2)
+        task.delete()
+        self.assertEqual(task.id, None)
         with self.assertRaises(Tasks.DoesNotExist):
-            Tasks.objects.get(pk=8)
+            Tasks.objects.get(pk=2)
