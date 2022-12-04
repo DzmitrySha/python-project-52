818e82f8c2ae8f426c123fb41c684c4b2adedacc
DzmitrySha <ravenbydev@gmail.com>
2022-12-04 23:33:12 +0300

    update user tests and fixtures

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,6 @@
 import json
 from django.test import TestCase
+from django.urls import reverse_lazy
 from users.models import User
 
 
@@ -13,7 +14,6 @@ class UsersTest(TestCase):
         self.assertEqual(user.username, user_data.get('username'))
         self.assertEqual(user.first_name, user_data.get('first_name'))
         self.assertEqual(user.last_name, user_data.get('last_name'))
-        self.assertEqual(user.email, user_data.get('email'))
 
     def test_update_user(self):
         user_data = json.load(open("fixtures/one_user.json"))
@@ -26,12 +26,10 @@ class UsersTest(TestCase):
         user1.username = user_data.get('username')
         user1.first_name = user_data.get('first_name')
         user1.last_name = user_data.get('last_name')
-        user1.email = user_data.get('email')
 
         self.assertEqual(user1.username, user_data.get('username'))
         self.assertEqual(user1.first_name, user_data.get('first_name'))
         self.assertEqual(user1.last_name, user_data.get('last_name'))
-        self.assertEqual(user1.email, user_data.get('email'))
 
     def test_delete_user(self):
         user = User.objects.get(pk=2)
@@ -40,3 +38,47 @@ class UsersTest(TestCase):
         self.assertEqual(user.pk, None)
         with self.assertRaises(User.DoesNotExist):
             User.objects.get(pk=2)
+
+
+class BaseTest(TestCase):
+    def setUp(self) -> None:
+        self.register_url = reverse_lazy('create')
+        self.user = {
+            'username': 'helena',
+            'first_name': 'Helen',
+            'last_name': 'Barrett',
+            'password': 'BarHe818',
+            'password2': 'BarHe818'
+        }
+        self.user_short_password = {
+            'username': 'helena',
+            'first_name': 'Helen',
+            'last_name': 'Barrett',
+            'password': '111',
+            'password2': '111'
+        }
+        return super().setUp()
+
+
+class RegisterUserTest(BaseTest):
+
+    def test_view_page_correctly(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/form.html')
+
+    def test_get_users(self):
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_user(self):
+        response = self.client.post(
+            self.register_url, self.user, format='text/html'
+        )
+        self.assertEqual(response.status_code, 200)  # 302
+
+    def test_cant_register_user_with_short_password(self):
+        response = self.client.post(
+            self.register_url, self.user_short_password, format='text/html'
+        )
+        self.assertEqual(response.status_code, 200)  # 400
