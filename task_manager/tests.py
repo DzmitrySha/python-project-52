d604aaadf5e4a08bbb6b23dbf38a9fb6db45b19e
DzmitrySha <ravenbydev@gmail.com>
2022-12-13 00:32:32 +0300

    add open homepage test

---
+++ task_manager/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class TestHomePage(TestCase):
+
+    def test_open_home_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
