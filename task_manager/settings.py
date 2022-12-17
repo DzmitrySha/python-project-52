fcb348ed7cbdd0bba1fbe48c29e9f0e1998cd420
DzmitrySha <ravenbydev@gmail.com>
2022-12-17 23:04:00 +0300

    refactor databases configs

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,17 +94,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
-
-# Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -122,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa E501
     },
 ]
-
 
 # Internationalization
 LANGUAGE_CODE = 'ru-ru'
