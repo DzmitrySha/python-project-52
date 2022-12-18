21533dc3adea8840a6627ecb5e556724a858ba19
DzmitrySha <ravenbydev@gmail.com>
2022-12-18 22:25:17 +0300

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,14 +95,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": 'django.db.backends.sqlite3',
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": 'django.db.backends.sqlite3',
+#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
+# DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600)
+}
 
 AUTH_USER_MODEL = 'users.User'
 
