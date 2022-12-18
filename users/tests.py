4e0cb42550038393c33ddb5e4e56e4b121bbb38f
DzmitrySha <ravenbydev@gmail.com>
2022-12-18 21:27:56 +0300

    update users tests

--- users/tests.py
+++ users/tests.py
@@ -5,14 +5,25 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse_lazy
 from task_manager.settings import FIXTURE_DIRS
+from users.forms import UserCreationFormCustom
 
 
-class TestCreateUser(TestCase):
+class SetupTestUser(TestCase):
+    fixtures = ['users.json']
 
     def setUp(self):
         self.create_url = reverse_lazy('create')
+        self.update_url = reverse_lazy('update', kwargs={"pk": 1})
+        self.delete_url = reverse_lazy("delete", kwargs={"pk": 1})
+        self.login_url = reverse_lazy('login')
+        self.users = get_user_model().objects.all()
+        self.user = get_user_model().objects.get(pk=1)
+        self.user2 = get_user_model().objects.get(pk=2)
         self.test_user = json.load(
             open(os.path.join(FIXTURE_DIRS[0], "one_user.json")))
+
+
+class TestCreateUser(SetupTestUser):
 
     def test_open_create_page(self):
         response = self.client.get(self.create_url)
@@ -21,24 +32,39 @@ class TestCreateUser(TestCase):
     def test_register_user(self):
         response = self.client.post(path=self.create_url, data=self.test_user)
         self.assertEqual(response.status_code, 302)
-        self.user = get_user_model().objects.get(pk=1)
+        self.user = get_user_model().objects.get(pk=4)
         self.assertEqual(first=self.user.username,
-                         second=self.test_user.get('username')
-                         )
+                         second=self.test_user.get('username'))
+
+        self.assertEqual(first=self.user.first_name,
+                         second=self.test_user.get('first_name'))
+        self.assertEqual(first=self.user.last_name,
+                         second=self.test_user.get('last_name'))
 
 
-class TestUpdateUser(TestCase):
-    fixtures = ['users.json']
+class TestUserCreationForm(SetupTestUser):
 
-    def setUp(self) -> None:
-        self.update_url = reverse_lazy('update', kwargs={"pk": 1})
-        self.user = get_user_model().objects.get(pk=1)
-        self.test_user = json.load(
-            open(os.path.join(FIXTURE_DIRS[0], "one_user.json")))
+    def test_user_create_form_with_data(self):
+        user_form = UserCreationFormCustom(data=self.test_user)
+        self.assertTrue(user_form.is_valid())
+        self.assertEqual(len(user_form.errors), 0)
+
+    def test_user_creation_form_with_no_data(self):
+        user_form = UserCreationFormCustom(data={})
+        self.assertFalse(user_form.is_valid())
+        self.assertEqual(len(user_form.errors), 5)
+
+
+class TestUpdateUser(SetupTestUser):
 
     def test_open_update_page(self):
+        # with no login
         response = self.client.get(self.update_url)
-        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.login_url, 302)
+        # with login
+        self.client.force_login(self.user)
+        response = self.client.get(self.update_url)
+        self.assertEqual(response.status_code, 200)
 
     def test_update_user(self):
         self.client.force_login(self.user)
@@ -48,21 +74,37 @@ class TestUpdateUser(TestCase):
         self.user = get_user_model().objects.get(pk=1)
         self.assertEqual(self.user.username, self.test_user.get("username"))
 
+    def test_update_other_user(self):
+        self.client.force_login(self.user2)
+        response = self.client.post(self.update_url, data=self.test_user)
+        self.assertEqual(response.status_code, 302)
 
-class TestDeleteUser(TestCase):
-    fixtures = ['users.json']
 
-    def setUp(self) -> None:
-        self.delete_url = reverse_lazy("delete", kwargs={"pk": 1})
-        self.test_user = get_user_model().objects.get(pk=1)
+class TestDeleteUser(SetupTestUser):
 
     def test_open_delete_page(self):
+        # with no login
+        response = self.client.get(self.delete_url)
+        self.assertRedirects(response, self.login_url, 302)
+        # with login (self delete user)
+        self.client.force_login(self.user)
+        response = self.client.get(self.delete_url)
+        self.assertEqual(response.status_code, 200)
+        # with login (delete other user)
+        self.client.force_login(self.user2)
         response = self.client.get(self.delete_url)
         self.assertEqual(response.status_code, 302)
 
     def test_delete_user(self):
-        self.client.force_login(self.test_user)
+        self.client.force_login(self.user)
         response = self.client.delete(self.delete_url)
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.users.count(), 2)
         with self.assertRaises(get_user_model().DoesNotExist):
             get_user_model().objects.get(pk=1)
+
+    def test_delete_other_user(self):
+        self.client.force_login(self.user2)
+        response = self.client.post(self.delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.users.count(), 3)
