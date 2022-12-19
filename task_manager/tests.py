f931dc71f02eb06546309f593d4c59d3cf1885b5
DzmitrySha <ravenbydev@gmail.com>
2022-12-20 00:52:17 +0300

    update main page tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -6,3 +6,8 @@ class TestHomePage(TestCase):
     def test_open_home_page(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
+
+    def test_find_in_html(self):
+        response = self.client.get('')
+        self.assertContains(response, 'Менеджер задач', status_code=200)
+        self.assertTemplateUsed(response, 'index.html')
