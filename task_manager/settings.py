3d489dbda208a381d57ef13546baa97e2ffdd9dc
DzmitrySha <ravenbydev@gmail.com>
2022-12-22 00:11:05 +0300

    remove unused code

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,16 +94,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": 'django.db.backends.sqlite3',
-#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
-
 DATABASES = {
     "default": dj_database_url.config(conn_max_age=600)
 }
