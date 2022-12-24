1f6b31df5f467101d4683e7e15320f5d1860116a
DzmitrySha <ravenbydev@gmail.com>
2022-12-24 16:18:39 +0300

    refactor UserCreationFormCustom

--- users/forms.py
+++ users/forms.py
@@ -1,32 +1,14 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-User = get_user_model()
 
 
 class UserCreationFormCustom(UserCreationForm):
-    first_name = forms.CharField(
-        label=_("First name"),
-        widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'type': 'text',
-                   'required': 'true',
-                   }),
-        help_text=_("Enter user first name.")
-    )
 
-    last_name = forms.CharField(
-        label=_("Last name"),
-        widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'type': 'text',
-                   'required': 'true',
-                   }),
-        help_text=_("Enter user last name.")
-    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
 
     class Meta:
-        model = User
-        fields = UserCreationForm.Meta.fields + ('username',
-                                                 'first_name', 'last_name')
+        model = get_user_model()
+        fields = ['username', 'first_name', 'last_name']
