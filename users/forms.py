ec70ac4bb4ecb46a467b7d9b66a27165f7bf72c2
DzmitrySha <ravenbydev@gmail.com>
2022-12-28 22:28:10 +0300

    add help_text to form fields

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class UserCreationFormCustom(UserCreationForm):
@@ -8,6 +9,8 @@ class UserCreationFormCustom(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
+        self.fields['first_name'].help_text = _("Enter user first name.")
+        self.fields['last_name'].help_text = _("Enter user last name.")
 
     class Meta:
         model = get_user_model()
